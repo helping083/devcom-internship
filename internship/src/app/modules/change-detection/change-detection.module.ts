3217f75a1bbc68ae56ced0a10df3732ef04cc700
helping083 <helping083@gmail.com>
2021-11-02 16:39:58 +0200

    adding a new post by using event emitter

--- internship/src/app/modules/change-detection/change-detection.module.ts
+++ internship/src/app/modules/change-detection/change-detection.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionPageComponent } from './change-detection-page.component';
 import { ChangeDetectionRoutingModule } from './change-detection-routing.module';
 import { PostCardModule } from 'src/app/shared/components/posts-card/post-card.module';
+import { AddPostFormComponent } from './shared/components/add-post-form/add-post-form.component';
+import { FormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [
-    ChangeDetectionPageComponent
+    ChangeDetectionPageComponent,
+    AddPostFormComponent
   ],
   imports: [
-    CommonModule, ChangeDetectionRoutingModule, PostCardModule
+    CommonModule, ChangeDetectionRoutingModule, PostCardModule, FormsModule
   ]
 })
 export class ChangeDetectionModule { }
