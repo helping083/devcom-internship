131208b7c27df45daad9f2f0f81a5c6e21b4889b
oleh.voitynskyi <oleh.voitynskyi@devcom.com>
2021-11-08 11:27:06 +0200

    attributive directive

--- internship/src/app/modules/attributive-structural/shared/directives/expandable.directive.ts
+++ internship/src/app/modules/attributive-structural/shared/directives/expandable.directive.ts
@@ -1,11 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 //do i have to create a module for each directive here
 @Directive({
   selector: '[appExpandable]'
 })
 export class ExpandableDirective {
+  @Input() expectedClassList: Array<string> = [];
 
-  constructor() { }
+  constructor(private readonly _el: ElementRef, private readonly _renderer: Renderer2) { 
+  }
 
+  @HostListener('focus')
+  public onFocus(): void {
+    const element = this._el.nativeElement;
+    this.expectedClassList.forEach(cssClass => this._renderer.addClass(element, cssClass));
+  }
+
+  @HostListener('blur')
+  public onBlur(): void {
+    const element = this._el.nativeElement;
+    this.expectedClassList.forEach(cssClass => this._renderer.removeClass(element, cssClass));
+  }
 }
