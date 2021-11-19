4c4a3f1cc193388623e16c6f461742e27184122d
oleh.voitynskyi <oleh.voitynskyi@devcom.com>
2021-11-19 11:02:17 +0200

    subjects init

---
+++ internship/src/app/modules/subjects/subjects.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SubjectsPageComponent } from './subjects-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    SubjectsPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SubjectsModule { }
