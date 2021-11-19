ea7652b64f9c97064826a69bf03503ad6f784ad3
oleh.voitynskyi <oleh.voitynskyi@devcom.com>
2021-11-19 11:04:34 +0200

    crlf

--- internship/src/app/modules/attributive-structural/shared/directives/expandable.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ExpandableDirective } from './expandable.directive';
-
-describe('ExpandableDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ExpandableDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- internship/src/app/modules/attributive-structural/shared/directives/ifelse.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IfelseDirective } from './ifelse.directive';
-
-describe('IfelseDirective', () => {
-  it('should create an instance', () => {
-    const directive = new IfelseDirective();
-    expect(directive).toBeTruthy();
-  });
-});
